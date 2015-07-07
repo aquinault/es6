967fb1b6456b873aad406d2c5500314c7c718d26
unknown <97494h@PWF00685.maif.local>
2015-07-07 12:09:01 +0200

    'refactor'

---
+++ es5/arrowFunctions.js
@@ -0,0 +1,6 @@
+"use strict";
+
+[1, 2, 3].map(function (x) {
+  return x * 2;
+});
+//# sourceMappingURL=arrowFunctions.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,38 @@
+// Various helper modules
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+
+var path = require('path');
+
+// I manage the paths of this project via an object
+var paths = {
+    es6: ['es6/**/*.js'],
+    es5: 'es5',
+    // Must be absolute or relative to source map
+    sourceRoot: path.join(__dirname, 'es6'),
+};
+
+// First task: transpile the ES6 code
+gulp.task('babel', function () { // (A)
+    return gulp.src(paths.es6)
+        .pipe(sourcemaps.init()) // (B)
+        .pipe(babel())
+        .pipe(sourcemaps.write('.', // (C)
+                  { sourceRoot: paths.sourceRoot }))
+        .pipe(gulp.dest(paths.es5));
+});
+
+// Second task: watch files, transpile if one of them changes
+gulp.task('watch', function() { // (D)
+    gulp.watch(paths.es6, ['babel']);
+});
+
+// The default task is 'watch'
+gulp.task('default', ['babel', 'watch']); // (E)
+
+
+/*
 var gulp = require("gulp");
 var babel = require("gulp-babel");
 var babelify = require('babelify');
@@ -29,3 +64,4 @@ gulp.task('default', function() {
     .pipe(source('main.js'))
     .pipe(gulp.dest('./dist'));
 });
+*/
