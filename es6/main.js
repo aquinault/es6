1a937e7912c7f9f9107491912b851fb520ed39b3
unknown <97494h@PWF00685.maif.local>
2015-07-17 10:27:37 +0200

    Add JWT

--- es6/main.js
+++ es6/main.js
@@ -36,21 +36,50 @@ server.use(restify.queryParser());
 server.use(restify.bodyParser());
 server.pre(restify.pre.sanitizePath());
 
-server.get('/posts/', function (req, res, next) {
+
+// Restify JWT
+// ---------------------------------------------------------------
+var jwt = require('restify-jwt');
+var jwttoken = require('jsonwebtoken');
+
+
+// API AUTH
+// ---------------------------------------------------------------
+server.get('/token', (req, res) => {
+	var secret = 'shhhhhhared-secret';
+	var token = jwttoken.sign({foo: 'bar', admin: true}, secret);
+
+    res.send(token);
+    return next();
+  });
+
+
+server.get('/protected', jwt({secret: 'shhhhhhared-secret'}), function(req, res) {
+	/* req.headers.authorization = 'Bearer ' + token;*/
+	console.log(req.user);
+	res.send(req.user);
+    /*if (!req.user.admin) return res.send(401);
+    res.send(200);*/
+  });
+
+
+// API POSTS
+// ---------------------------------------------------------------
+server.get('/posts/', (req, res, next) => {
 	co(function *(){
 		UsersController.list(req, res, next);
 		return next();
 	 }).catch(onerror); 	
 });
 
-server.get('/posts/:id', function (req, res, next) {
+server.get('/posts/:id', (req, res, next) => {
 	co(function *(){
 		UsersController.get(req, res, next);
 		return next();
 	 }).catch(onerror); 	
 });
 
-server.put('/posts/:id', function (req, res, next) {
+server.put('/posts/:id', (req, res, next) => {
 	co(function *(){
 		UsersController.update(req, res, next);
 		return next();
@@ -58,14 +87,14 @@ server.put('/posts/:id', function (req, res, next) {
 });
 
 
-server.del('/posts/', function (req, res, next) {
+server.del('/posts/', (req, res, next) => {
 	co(function *(){		
 		UsersController.removeAll(req, res, next);
 		return next();
 	 }).catch(onerror); 	
 });
 
-server.del('/posts/:id', function (req, res, next) {
+server.del('/posts/:id', (req, res, next) => {
 	co(function *(){		
 		UsersController.remove(req, res, next);
 		return next();
@@ -73,7 +102,7 @@ server.del('/posts/:id', function (req, res, next) {
 });
 
 
-server.post('/posts/', function (req, res, next) {
+server.post('/posts/', (req, res, next) => {
 	co(function *(){
 		UsersController.create(req, res, next);
 		return next();
@@ -81,21 +110,22 @@ server.post('/posts/', function (req, res, next) {
 });
 
 
-
-server.get('/echo2/:name', function (req, res, next) {
+// SOME STUFF
+// ---------------------------------------------------------------
+server.get('/echo2/:name', (req, res, next) => {
 	res.send(req.params); 
 	return next();
 });
 
-server.listen(8080, function () {
+server.listen(8080, () => {
 	console.log('%s listening at %s', server.name, server.url);
 });
 
-process.on( 'SIGINT', function() {
+process.on( 'SIGINT', () => {
 	console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
 
 	// Disconnect Mongo
-	service1.exit();
+	//service1.exit();
 
 	// some other closing procedures go here
 	process.exit( );
