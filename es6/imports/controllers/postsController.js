ae51734e4b0e07738d5bec1d926935db30dbfffd
Anthony <aquinault@gmail.com>
2015-07-17 23:53:04 +0200

    user agent parser

--- es6/imports/controllers/postsController.js
+++ es6/imports/controllers/postsController.js
@@ -20,6 +20,25 @@ PostsController.create = (req, res, next) => {
 
 PostsController.list = (req, res, next) => {
   Co(function* () {
+    //----------------
+    var infos = {
+      agent: req.headers['user-agent'], // User Agent we get from headers
+      referrer: req.headers['referrer'], //  Likewise for referrer
+      ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress, // Get IP - allow for proxy
+      screen: { // Get screen info that we passed in url post data
+        width: req.params.width,
+        height: req.params.height
+      }
+    }
+    console.log(infos);
+
+    var UAParser = require('ua-parser-js');
+    var parser = new UAParser();
+    var ua = req.headers['user-agent'];     // user-agent header from an HTTP request
+    console.log(parser.setUA(ua).getResult());
+    //----------------
+
+
     let results = yield Post.find().exec();
     console.log('posts list');
     res.send(results);
